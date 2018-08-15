13cf453ff213841574dbed0d9228eb1335dd5102
Shubham-tiwari123 <st540362@gmail.com>
2018-08-15 13:39:56 +0530

    main file

--- BTreeConstruction.java
+++ BTreeConstruction.java
@@ -10,7 +10,7 @@ public class BTreeConstruction {
         ConstructionQuestion tree = new ConstructionQuestion();
        
         do{
-            System.out.print("\n1)Create tree using inorder and preorder\n"
+            System.out.print(/*"\n1)Create tree using inorder and preorder\n"
                     + "2)Create tree from linlist\n3)Create tree from array"
                     + "\n4)Convert tree to DLL\n5)Convert a given tree to its Sum Tree\n"
                     + "6)Convert tree to Circular DLL\n7)Create a Doubly Linked "
@@ -18,7 +18,9 @@ public class BTreeConstruction {
                     + "from given preorder and postorder traversals\n"
                     + "9)Binary tree to a tree that holds Logical AND property\n"
                     + "10)Binary Tree into Doubly Linked List in spiral fashion\n"
-                    + "12)Exit");
+                    + "11)Mirror Tree\n12)Ternary Expression to a Binary Tree\n"
+                    + "13)Flip Binary Tree\n"+*/"14)every node stores sum of all"
+                    + " nodes in left subtree\n20)Exit");
             System.out.print("\nEnter your choice:-");
             choice=sc.nextInt();
             switch(choice){
@@ -118,8 +120,53 @@ public class BTreeConstruction {
                         }
                         tree.printSpiral(tree.getRootData());
                         break;
+                        
+                case 11:System.out.print("\nNumber of nodes:-");
+                        num=sc.nextInt();
+                        for(int i=0;i<num;i++){
+                            System.out.print("\nValue:-");
+                            int value = sc.nextInt();
+                            tree.createBT(value);
+                        }
+                        tree.mirrorTree(tree.getRootData());
+                        System.out.print("\nPrinting mirror tree...");
+                        tree.inorder(tree.getRootData());
+                        break;
+                        
+                case 12:String exp = "a?b?c:d:e";
+                        char[] expression = exp.toCharArray();
+                        CharNode charNode=tree.ternaryToTree(expression,0);
+                        tree.printTernary(charNode);
+                        break;
+                        
+                case 13:System.out.print("\nNumber of nodes:-");
+                        num=sc.nextInt();
+                        for(int i=0;i<num;i++){
+                            System.out.print("\nValue:-");
+                            int value = sc.nextInt();
+                            tree.createBT(value);
+                        }
+                        System.out.print("\nBefore flip....\n");
+                        tree.inorder(tree.getRootData());
+                        System.out.print("\nAfter flip...\n");
+                        tree.flipBinaryTree(tree.getRootData());
+                        tree.inorder(tree.getRootData());
+                        break;
+                        
+                case 14:System.out.print("\nNumber of nodes:-");
+                        num=sc.nextInt();
+                        for(int i=0;i<num;i++){
+                            System.out.print("\nValue:-");
+                            int value = sc.nextInt();
+                            tree.createBT(value);
+                        }
+                        System.out.print("\nBefor update.....\n");
+                        tree.inorder(tree.getRootData());
+                        tree.updateTree(tree.getRootData());
+                        System.out.print("\nAfter update.....\n");
+                        tree.inorder(tree.getRootData());
             }
-        }while(choice!=12);
+        }while(choice!=20);
     }
     
 }
